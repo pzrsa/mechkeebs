c8b8a185f4cad4f0f2fc3ea0918c40873f40dfdf
parsa <parsamesg@gmail.com>
2021-11-13 20:23:57 +0000

    add express to project

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import express, { Application } from "express";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 const main = async () => {
   const conn = await createConnection();
+  const app: Application = express();
+
   console.log(conn.isConnected);
+
+  app.listen(3000, () => {
+    console.log("server up on http://localhost:3000");
+  });
 };
 
 main().catch((err) => console.error(err));
