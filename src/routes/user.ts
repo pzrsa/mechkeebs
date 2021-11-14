4344553104f67b93c17a530d85ecf36f5aba750f
parsa <parsamesg@gmail.com>
2021-11-14 20:12:37 +0000

    create register user endpoint for adding a new user to db

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,20 +5,36 @@ const userRouter: Router = router();
 
 userRouter.get("/users", async (_, res: Response) => {
   const results = await User.find({
-    select: ["id", "username", "createdAt", "updatedAt"],
     relations: ["setups"],
   });
 
-  return res.status(200).json({ users: results });
+  return res.status(200).json({ results });
 });
 
 userRouter.get("/users/:id", async (req: Request, res: Response) => {
   const result = await User.findOne(req.params.id, {
-    select: ["id", "username", "createdAt", "updatedAt"],
     relations: ["setups"],
   });
 
-  return res.json({ user: result });
+  return res.status(200).json({ result });
+});
+
+userRouter.post("/register", async (req: Request, res: Response) => {
+  try {
+    const result = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    }).save();
+    return res
+      .status(200)
+      .json({ result: `User '${result.username}' created` });
+  } catch (err) {
+    if (err.code === "23505") {
+      return res.status(400).json({ error: "Username or Email aready taken" });
+    }
+    return res.status(400).json({ error: err.detail });
+  }
 });
 
 export default userRouter;
