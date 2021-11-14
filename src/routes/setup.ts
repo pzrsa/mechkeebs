d638474bb1747c1fd0380a197a914b641a3aa698
parsa <parsamesg@gmail.com>
2021-11-14 23:40:24 +0000

    create route to get setup by id (also debating whether i should display creator details)

--- src/routes/setup.ts
+++ src/routes/setup.ts
@@ -1,16 +1,20 @@
-import router, { Response, Router } from "express";
-import { getRepository } from "typeorm";
+import router, { Request, Response, Router } from "express";
 import { Setup } from "../entity/Setup";
 
 const setupRouter: Router = router();
 
 setupRouter.get("/setups", async (_, res: Response) => {
-  const results = await getRepository(Setup)
-    .createQueryBuilder()
-    .loadAllRelationIds()
-    .getMany();
+  const results = await Setup.find({ relations: ["creator"] });
 
   return res.status(200).json({ results });
+});
+
+setupRouter.get("/setups/:id", async (req: Request, res: Response) => {
+  const result = await Setup.findOne(req.params.id, {
+    relations: ["creator"],
+  });
+
+  return res.status(200).json({ result });
 });
 
 export default setupRouter;
