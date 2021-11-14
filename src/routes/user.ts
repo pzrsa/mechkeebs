f7f10ee1b2a9d0d77a9e8ee069c2d31e457f22c8
parsa <parsamesg@gmail.com>
2021-11-14 10:51:41 +0000

    create GET routes for user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,24 @@
-import router, { Response, Router } from "express";
+import router, { Request, Response, Router } from "express";
 import { User } from "../entity/User";
 
 const userRouter: Router = router();
 
 userRouter.get("/users", async (_, res: Response) => {
-  const users = await User.find();
+  const results = await User.find({
+    select: ["id", "username", "createdAt", "updatedAt"],
+    relations: ["setups"],
+  });
 
-  return res.json({ users: users });
+  return res.status(200).json({ users: results });
+});
+
+userRouter.get("/users/:id", async (req: Request, res: Response) => {
+  const result = await User.findOne(req.params.id, {
+    select: ["id", "username", "createdAt", "updatedAt"],
+    relations: ["setups"],
+  });
+
+  return res.json({ user: result });
 });
 
 export default userRouter;
