8c384fc6345592b471c96582909d420102040a51
parsa <parsamesg@gmail.com>
2021-11-15 10:53:46 +0000

    create post route for creating setups

--- src/routes/setup.ts
+++ src/routes/setup.ts
@@ -4,17 +4,29 @@ import { Setup } from "../entity/Setup";
 const setupRouter: Router = router();
 
 setupRouter.get("/setups", async (_, res: Response) => {
-  const results = await Setup.find({ relations: ["creator"] });
+  const results = await Setup.find();
 
   return res.status(200).json({ results });
 });
 
 setupRouter.get("/setups/:id", async (req: Request, res: Response) => {
-  const result = await Setup.findOne(req.params.id, {
-    relations: ["creator"],
-  });
+  const result = await Setup.findOne(req.params.id);
 
   return res.status(200).json({ result });
+});
+
+setupRouter.post("/setups/create", async (req: Request, res: Response) => {
+  try {
+    const result = await Setup.create({
+      title: req.body.title,
+      description: req.body.description,
+      items: req.body.items,
+      creatorId: req.body.creatorId,
+    }).save();
+    return res.status(200).json({ result });
+  } catch (err) {
+    return res.status(400).json({ error: err.detail });
+  }
 });
 
 export default setupRouter;
