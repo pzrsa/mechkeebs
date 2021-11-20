67d64c774f94dc2ca366dcd251aaaef32d27a3e4
parsa <parsamesg@gmail.com>
2021-11-20 11:22:03 +0000

    create login function to fetch access for logging in

---
+++ web/src/lib/loginUser.ts
@@ -0,0 +1,18 @@
+import LoginFormValues from "../interfaces/LoginFormValues";
+
+const loginUser = async (values: LoginFormValues) => {
+  const response = await fetch("http://localhost:4000/api/users/login", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ...values }),
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export default loginUser;
