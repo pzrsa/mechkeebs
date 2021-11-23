23d366ebf63fa3337b5491e95b4dcfb614144779
parsa <parsamesg@gmail.com>
2021-11-23 10:53:23 +0000

    render logged in navbar with username and logout button

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,26 +1,55 @@
 import { Box, Flex, Heading, Link } from "@chakra-ui/layout";
 import NextLink from "next/link";
 import React from "react";
+import logoutUser from "../lib/logoutUser";
+import useUser from "../lib/useUser";
 
 interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
+  const { user, mutate } = useUser();
+
+  let body;
+
+  if (!user) {
+    body = "loading...";
+  } else if (!user?.user) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <>
+        <Link mr={2}>{user.user.username}</Link>
+        <Link
+          onClick={() => {
+            logoutUser();
+            mutate(null, true);
+          }}
+        >
+          Logout
+        </Link>
+      </>
+    );
+    console.log(user);
+  }
+
   return (
-    <Flex zIndex={1} position="sticky" top={0} p={4}>
+    <Flex zIndex={1} bg="tan" position="sticky" top={0} p={4}>
       <Flex flex={1} m="auto" align="center" maxW={800}>
         <NextLink href="/">
           <Link>
             <Heading>SetupScope</Heading>
           </Link>
         </NextLink>
-        <Box ml={"auto"}>
-          <NextLink href="/login">
-            <Link mr={2}>Login</Link>
-          </NextLink>
-          <NextLink href="/register">
-            <Link>Register</Link>
-          </NextLink>
-        </Box>
+        <Box ml={"auto"}>{body}</Box>
       </Flex>
     </Flex>
   );
