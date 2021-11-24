351787c44e49d142becc22a6bb375972b4390b39
parsa <parsamesg@gmail.com>
2021-11-24 21:45:36 +0000

    create create setup function

---
+++ web/src/lib/createSetup.ts
@@ -0,0 +1,26 @@
+import PostSetupFormValues from "../interfaces/PostSetupFormValues";
+
+const createSetup = async (
+  title: PostSetupFormValues["title"],
+  items: string[],
+  creatorId: number
+) => {
+  const response = await fetch("http://localhost:4000/api/setups/create", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      items,
+      creatorId,
+    }),
+  });
+
+  const result = await response.json();
+
+  return result;
+};
+
+export default createSetup;
