97b270a05b19a3807a026756d27780e5dd21b1dc
parsa <parsamesg@gmail.com>
2021-11-24 18:07:59 +0000

    add placeholder

--- web/src/components/MultiInputField.tsx
+++ web/src/components/MultiInputField.tsx
@@ -5,14 +5,18 @@ import React from "react";
 
 interface MultiInputFieldProps {
   name: string;
+  placeholder: string;
 }
 
-const MultiInputField: React.FC<MultiInputFieldProps> = ({ ...props }) => {
+const MultiInputField: React.FC<MultiInputFieldProps> = ({
+  placeholder,
+  ...props
+}) => {
   const [field, meta] = useField(props);
 
   return (
     <FormControl mb={5} isInvalid={!!(meta.touched && meta.error)}>
-      <Input {...field} {...props} id={field.name} />
+      <Input {...field} {...props} id={field.name} placeholder={placeholder} />
       {meta.touched && meta.error ? (
         <FormErrorMessage>{meta.error}</FormErrorMessage>
       ) : null}
