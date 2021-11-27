d7bf163397a36c7a53fd980b6bdec244b5c9ed3e
parsa <parsamesg@gmail.com>
2021-11-27 13:20:10 +0000

    remove comments

--- web/src/utils/fetcher.ts
+++ web/src/utils/fetcher.ts
@@ -1,4 +1,4 @@
-const fetcher = async (input: RequestInfo): Promise<JSON> => {
+const fetcher = async (input: RequestInfo) => {
   const response = await fetch(input, {
     method: "GET",
     credentials: "include",
@@ -7,12 +7,8 @@ const fetcher = async (input: RequestInfo): Promise<JSON> => {
     },
   });
 
-  // if the server replies, there's always some data in json
-  // if there's a network error, it will throw at the previous line
   const data = await response.json();
 
-  // response.ok is true when res.status is 2xx
-  // https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
   return data;
 };
 
