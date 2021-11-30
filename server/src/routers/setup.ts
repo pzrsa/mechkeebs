479f6eeea664818c434873e7752a2321af879912
parsa <parsamesg@gmail.com>
2021-11-30 14:31:44 +0000

    api to accept limit and cursor for pagination with query builder

--- server/src/routers/setup.ts
+++ server/src/routers/setup.ts
@@ -1,23 +1,37 @@
 import router, { Request, Response, Router } from "express";
+import { getConnection } from "typeorm";
 import { Setup } from "../entity/Setup";
 import { getSession } from "../utils/sessions";
 
 const setupRouter: Router = router();
 
-setupRouter.get("/setups", async (_, res: Response) => {
-  const results = await Setup.find({
-    order: { createdAt: "DESC" },
-    relations: ["creator"],
-  });
+setupRouter.get(
+  "/setups/:limit/:cursor?",
+  async (req: Request, res: Response) => {
+    const qb = getConnection()
+      .getRepository(Setup)
+      .createQueryBuilder("s")
+      .innerJoinAndSelect("s.creator", "u", "u.id = s.creatorId")
+      .take(parseInt(req.params.limit))
+      .orderBy("s.createdAt", "DESC");
 
-  return res.status(200).json({ results });
-});
+    if (req.params.cursor) {
+      qb.where("s.createdAt < :cursor", {
+        cursor: new Date(parseInt(req.params.cursor)),
+      });
+    }
 
-setupRouter.get("/setups/:id", async (req: Request, res: Response) => {
-  const result = await Setup.findOne(req.params.id);
+    const results = await qb.getMany();
 
-  return res.status(200).json({ result });
-});
+    return res.status(200).json({ results });
+  }
+);
+
+// setupRouter.get("/setups/:id", async (req: Request, res: Response) => {
+//   const result = await Setup.findOne(req.params.id);
+
+//   return res.status(200).json({ result });
+// });
 
 setupRouter.post("/setups/create", async (req: Request, res: Response) => {
   try {
