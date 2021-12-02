260b54bf66c83e7407cd4c14665f3d3a8f389dea
parsa <parsamesg@gmail.com>
2021-12-02 21:32:12 +0000

    add progress bar on each page change (like github one)

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,11 +1,35 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
-import React from "react";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import React, { useEffect } from "react";
 import { SWRConfig } from "swr";
 import Layout from "../components/Layout";
+import "../styles/nprogress.css";
 import fetcher from "../utils/fetcher";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   return (
     <SWRConfig value={{ fetcher }}>
       <ChakraProvider resetCSS>
