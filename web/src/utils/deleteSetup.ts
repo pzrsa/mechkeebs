475d7362cdbe1c06b2ae7a02d19aa449d61fde57
parsa <parsamesg@gmail.com>
2021-12-05 17:57:45 +0000

    not postId typo

--- web/src/utils/deleteSetup.ts
+++ web/src/utils/deleteSetup.ts
@@ -1,11 +1,11 @@
-const deleteSetup = async (postId: number) => {
+const deleteSetup = async (setupId: number) => {
   const response = await fetch("http://localhost:4000/api/setups/delete", {
     method: "DELETE",
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ postId }),
+    body: JSON.stringify({ setupId }),
   });
 
   const result = await response.json();
