3488ace0cedc46bea3c330ce06563d4d479b8bbf
parsa <parsamesg@gmail.com>
2021-12-13 18:52:41 +0000

    add usesetup hook for client side rendering of the setup in edit page

---
+++ web/src/utils/useSetup.ts
@@ -0,0 +1,12 @@
+import useSWR from "swr";
+import Setup from "../types/Setup";
+
+const useSetup = (setupId: number) => {
+  const { data, isValidating: loading } = useSWR<Setup>(
+    `http://localhost:4000/api/setups?id=${setupId}`
+  );
+
+  return { data, loading };
+};
+
+export default useSetup;
