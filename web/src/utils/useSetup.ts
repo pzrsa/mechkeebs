55b3ce15134f13bbb1493d14b4a933640d6cff66
parsa <parsamesg@gmail.com>
2021-12-20 11:19:51 +0000

    make usesetup as immutable swr call, meaning it wont revalidate again

--- web/src/utils/useSetup.ts
+++ web/src/utils/useSetup.ts
@@ -1,8 +1,8 @@
-import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 import Setup from "../types/Setup";
 
 const useSetup = (setupId: number) => {
-  const { data, isValidating: loading } = useSWR<Setup>(
+  const { data, isValidating: loading } = useSWRImmutable<Setup>(
     `http://localhost:4000/api/setups?id=${setupId}`
   );
 
