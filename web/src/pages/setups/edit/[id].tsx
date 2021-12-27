f964f91de58ca90e714e617bee4a7305a58d2b7c
parsa <parsamesg@gmail.com>
2021-12-27 17:02:55 +0000

    only show and edit setup if the creator

--- web/src/pages/setups/edit/[id].tsx
+++ web/src/pages/setups/edit/[id].tsx
@@ -21,6 +21,7 @@ import getSetupFromUrl from "../../../utils/getSetupFromUrl";
 import getUser from "../../../utils/getUser";
 import updateSetup from "../../../utils/updateSetup";
 import useSetup from "../../../utils/useSetup";
+import useUser from "../../../utils/useUser";
 import withAuth from "../../../utils/withAuth";
 
 interface EditSetupProps {}
@@ -29,116 +30,122 @@ const EditSetup: React.FC<EditSetupProps> = ({}) => {
   withAuth();
 
   const id = getSetupFromUrl();
-  const { data, loading } = useSetup(id);
+  const { data, loading: setupLoading } = useSetup(id);
+
+  const { user, loading: userLoading } = useUser();
 
   const initialValues: PostSetupFormValues = {
-    title: !loading && data?.result ? data.result.title : "",
+    title: !setupLoading && data?.result ? data.result.title : "",
     items:
-      !loading && data?.result
+      !setupLoading && data?.result
         ? data?.result?.items.map((itemName) => ({ item: itemName }))
         : [{ item: "" }, { item: "" }],
   };
 
-  if (!loading && !data?.result) return <Error statusCode={404} />;
+  if (!setupLoading && !data?.result) return <Error statusCode={404} />;
+
+  if (
+    !userLoading &&
+    !setupLoading &&
+    data?.result?.creatorId !== user?.user?.id
+  )
+    return <Error statusCode={401} />;
 
   return (
     <Wrapper>
       <Box width="750px" mx="auto">
-        <Heading my={5}>Edit a setup</Heading>
-        {loading ? (
+        {setupLoading ? (
           <Center>
             <Spinner size="xl" />
           </Center>
         ) : (
-          <Formik
-            initialValues={initialValues}
-            validationSchema={Yup.object().shape({
-              title: Yup.string()
-                .min(5, "Must be greater than 5 characters")
-                .required("Title required"),
+          <>
+            <Heading my={5}>Edit Setup</Heading>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={Yup.object().shape({
+                title: Yup.string()
+                  .min(5, "Must be greater than 5 characters")
+                  .required("Title required"),
 
-              items: Yup.array().of(
-                Yup.object().shape({
-                  item: Yup.string()
-                    .min(3, "Must be greater than 3 characters")
-                    .required("Item name required"),
-                })
-              ),
-            })}
-            onSubmit={async (values) => {
-              const { user } = await getUser();
+                items: Yup.array().of(
+                  Yup.object().shape({
+                    item: Yup.string()
+                      .min(3, "Must be greater than 3 characters")
+                      .required("Item name required"),
+                  })
+                ),
+              })}
+              onSubmit={async (values) => {
+                const { user } = await getUser();
 
-              const items = values["items"]!.map(
-                (item: any): string => item.item
-              );
+                const items = values["items"]!.map(
+                  (item: any): string => item.item
+                );
 
-              const response = await updateSetup(
-                data!.result.id,
-                values.title,
-                items,
-                user.id
-              );
+                const response = await updateSetup(
+                  data!.result.id,
+                  values.title,
+                  items,
+                  user.id
+                );
 
-              if (response?.error) {
-                console.error(response.error);
-              } else if (response?.result) {
-                router.push("/");
-              }
-            }}
-          >
-            {({ values, isSubmitting }) => (
-              <Form>
-                <InputField name="title" label="Title" />
-                <FieldArray
-                  name="items"
-                  render={(arrayHelpers) => (
-                    <Box>
-                      <Flex mb={5} mt={10} alignItems="center">
-                        <FormLabel>Items</FormLabel>
-                        <Button
-                          ml="auto"
-                          colorScheme="teal"
-                          type="button"
-                          onClick={() => arrayHelpers.unshift({ item: "" })}
-                        >
-                          Add item
-                        </Button>
-                      </Flex>
-                      {values.items.map((_, index) => (
-                        <Flex
-                          direction="row"
-                          justifyContent="center"
-                          key={index}
-                        >
-                          <MultiInputField
-                            name={`items.${index}.item`}
-                            placeholder="Item name"
-                          />
-                          {values.items.length <= 2 ? null : (
-                            <IconButton
-                              ml={5}
-                              aria-label="Remove item"
-                              icon={<MinusIcon />}
-                              colorScheme="red"
-                              type="button"
-                              onClick={() => arrayHelpers.remove(index)}
-                            />
-                          )}
+                if (response?.error) {
+                  console.error(response.error);
+                } else if (response?.result) {
+                  await router.push("/");
+                }
+              }}
+            >
+              {({ values, isSubmitting }) => (
+                <Form>
+                  <InputField name="title" label="Title" />
+                  <FieldArray
+                    name="items"
+                    render={(arrayHelpers) => (
+                      <Box>
+                        <Flex mb={5} mt={10} alignItems="center">
+                          <FormLabel>Items</FormLabel>
+                          <Button
+                            ml="auto"
+                            type="button"
+                            onClick={() => arrayHelpers.unshift({ item: "" })}
+                          >
+                            Add Item
+                          </Button>
                         </Flex>
-                      ))}
-                    </Box>
-                  )}
-                />
-                <Button
-                  colorScheme="teal"
-                  isLoading={isSubmitting}
-                  type="submit"
-                >
-                  Post Setup
-                </Button>
-              </Form>
-            )}
-          </Formik>
+                        {values.items.map((_, index) => (
+                          <Flex
+                            direction="row"
+                            justifyContent="center"
+                            key={index}
+                          >
+                            <MultiInputField
+                              name={`items.${index}.item`}
+                              placeholder="Item name"
+                            />
+                            {values.items.length <= 2 ? null : (
+                              <IconButton
+                                ml={5}
+                                aria-label="Remove item"
+                                icon={<MinusIcon />}
+                                colorScheme="red"
+                                type="button"
+                                onClick={() => arrayHelpers.remove(index)}
+                              />
+                            )}
+                          </Flex>
+                        ))}
+                      </Box>
+                    )}
+                  />
+                  <Button isLoading={isSubmitting} type="submit">
+                    Update
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </>
         )}
       </Box>
     </Wrapper>
--- web/src/utils/useSetup.ts
+++ web/src/utils/useSetup.ts
@@ -3,7 +3,8 @@ import Setup from "../types/Setup";
 
 const useSetup = (setupId: number) => {
   const { data, isValidating: loading } = useSWR<Setup>(
-    `http://localhost:4000/api/setups?id=${setupId}`
+    `http://localhost:4000/api/setups?id=${setupId}`,
+    { revalidateOnFocus: false }
   );
 
   return { data, loading };
