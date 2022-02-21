e4b39a47c08dd1367431baae422b34cceb12eb7c
parsa <parsamesg@gmail.com>
2022-02-21 21:26:03 +0000

    component for file input fully working

--- web/src/components/FileInputField.tsx
+++ web/src/components/FileInputField.tsx
@@ -1,17 +1,39 @@
-import { FormControl, FormLabel } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormErrorIcon,
+  FormErrorMessage,
+  FormLabel,
+} from "@chakra-ui/react";
 import { useField } from "formik";
+import { InputHTMLAttributes } from "react";
 
-interface FileInputFieldProps {
+type FileInputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
-}
+};
 
 const FileInputField: React.FC<FileInputFieldProps> = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
+  const [field, meta, helpers] = useField(props);
 
   return (
     <FormControl mb={5} isInvalid={!!(meta.touched && meta.error)}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <input
+        type="file"
+        id={field.name}
+        {...field}
+        {...props}
+        value={undefined}
+        onChange={(event) => {
+          helpers.setValue(event.currentTarget.files?.item(0));
+        }}
+      />
+      {meta.touched && meta.error ? (
+        <FormErrorMessage>
+          <FormErrorIcon />
+          {meta.error}
+        </FormErrorMessage>
+      ) : null}
     </FormControl>
   );
 };
