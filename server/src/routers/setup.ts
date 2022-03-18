c204189a53fe5079d978022a37309468057c0bd4
parsa <parsamesg@gmail.com>
2022-03-18 17:49:13 +0000

    make retrieving all setups simple for migrating to react query easier

--- server/src/routers/setup.ts
+++ server/src/routers/setup.ts
@@ -2,7 +2,6 @@ import { Storage } from "@google-cloud/storage";
 import router, { Request, Response, Router } from "express";
 import { UploadedFile } from "express-fileupload";
 import path from "path";
-import { getConnection } from "typeorm";
 import { Setup } from "../entity/Setup";
 import { getSession } from "../utils/sessions";
 
@@ -13,32 +12,10 @@ const storage = new Storage({
   projectId: "setupscope",
 });
 
-setupRouter.get("/setups", async (req: Request, res: Response) => {
-  const qb = getConnection()
-    .getRepository(Setup)
-    .createQueryBuilder("s")
-    .innerJoinAndSelect("s.creator", "u", "u.id = s.creatorId")
-    .orderBy("s.createdAt", "DESC");
+setupRouter.get("/setups", async (_: Request, res: Response) => {
+  const result = await Setup.find();
 
-  if (req.query.limit && req.query.cursor) {
-    qb.take(parseInt(req.query.limit as string));
-    qb.where("s.createdAt < :cursor", {
-      cursor: new Date(parseInt(req.query.cursor as string)),
-    });
-  }
-
-  if (req.query.id) {
-    try {
-      const result = await Setup.findOne(parseInt(req.query.id as string));
-      return res.status(200).json({ result });
-    } catch (err) {
-      return res.status(400).json({ error: err.detail });
-    }
-  }
-
-  const results = await qb.getMany();
-
-  return res.status(200).json({ results });
+  return res.status(200).json({ result });
 });
 
 setupRouter.post("/setups/create", async (req: Request, res: Response) => {
