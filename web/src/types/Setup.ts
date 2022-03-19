b6084a3aff70698bbbec69be28b057cdecd4874f
parsa <parsamesg@gmail.com>
2022-03-19 21:24:18 +0000

    move types around into files

--- web/src/types/Setup.ts
+++ web/src/types/Setup.ts
@@ -1,13 +1,29 @@
-type Setup = {
+export type Setups = {
+  result: [
+    {
+      id: number;
+      title: string;
+      imageName: string;
+      items: string[];
+      creatorId: number;
+      createdAt: string;
+      updatedAt: string;
+    }
+  ];
+};
+
+export type Setup = {
   result: {
     id: number;
     title: string;
     items: string[];
     creatorId: number;
-    creator: { username: string };
     createdAt: string;
     updatedAt: string;
   };
 };
 
-export default Setup;
+export interface SetupFormValues {
+  title: string;
+  items: object[];
+}
