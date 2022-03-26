6bd74b6a057317cb1bf69a73a49a0c0042b1a9fb
parsa <parsamesg@gmail.com>
2022-03-26 20:03:32 +0000

    loginuser custom hook

--- web/src/lib/mutations.ts
+++ web/src/lib/mutations.ts
@@ -1,3 +1,5 @@
+import { LoginFormValues } from "../types/User";
+
 export const createSetup = async (formData: FormData) => {
   const response = await fetch("http://localhost:4000/api/setups/create", {
     method: "POST",
@@ -8,6 +10,19 @@ export const createSetup = async (formData: FormData) => {
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
+
+  return response.json();
+};
+
+export const loginUser = async (loginInfo: LoginFormValues) => {
+  const response = await fetch("http://localhost:4000/api/users/login", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ...loginInfo }),
+  });
 
   return response.json();
 };
