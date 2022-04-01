0c5fbfab459bd589a893744b00035bcf6e71f632
parsa <parsamesg@gmail.com>
2022-04-01 11:11:36 +0100

    create keyboard entity for relation to posts

---
+++ server/src/entity/Keyboard.ts
@@ -0,0 +1,42 @@
+import {
+  BaseEntity,
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity({ name: "keyboards" })
+export class Keyboard extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  switches: string;
+
+  @Column()
+  keycaps: string;
+
+  @Column()
+  stabilizers: string;
+
+  @Column({ name: "created_at" })
+  createdAt: number;
+
+  @Column({ name: "updated_at", nullable: true })
+  updatedAt: number;
+
+  @BeforeInsert()
+  public setCreatedAt() {
+    this.createdAt = Math.floor(Date.now() / 1000);
+  }
+
+  @BeforeUpdate()
+  public setUpdatedAt() {
+    this.updatedAt = Math.floor(Date.now() / 1000);
+  }
+}
