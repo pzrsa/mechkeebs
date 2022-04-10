8d5ccbbd13cd5464c2304c189db8b23165a9c59c
parsa <parsamesg@gmail.com>
2022-04-10 19:51:05 +0100

    update to display all setups in card format with load more button

--- web/src/components/Posts.tsx
+++ web/src/components/Posts.tsx
@@ -1,4 +1,13 @@
-import { Button, Text } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Box,
+  Button,
+  Center,
+  Heading,
+  SimpleGrid,
+  Spinner,
+} from "@chakra-ui/react";
+import NextImage from "next/image";
 import React from "react";
 import { usePaginatedPosts } from "../hooks/post";
 
@@ -16,69 +25,54 @@ const Posts: React.FC<PostsProps> = ({}) => {
     isReachingEnd,
   } = usePaginatedPosts();
 
-  if (isLoading) return <>loading...</>;
-  let totalPosts = 0;
-  for (let i = 0; i < posts!.length; i++) {
-    totalPosts += posts![i].result.length;
+  if (isLoading) {
+    return (
+      <Center>
+        <Spinner size={"xl"} />
+      </Center>
+    );
+  } else if (!posts?.length) {
+    return (
+      <Center>
+        <Heading size={"md"}>No posts to display.</Heading>
+      </Center>
+    );
   }
-
-  // let body;
-  // if (isLoading) {
-  //   body = (
-  //     <Center>
-  //       <Spinner size={"xl"} />
-  //     </Center>
-  //   );
-  // } else if (!posts?.result.length) {
-  //   body = (
-  //     <Center>
-  //       <Heading size={"md"}>No posts to display.</Heading>
-  //     </Center>
-  //   );
-  // }
-  // if (posts?.result.length) {
-  //   body = (
-  //     <SimpleGrid minChildWidth={"350px"} spacing={4}>
-  //       {posts.result.map((post) => (
-  //         <Box
-  //           overflow={"hidden"}
-  //           borderRadius={"md"}
-  //           shadow={"md"}
-  //           key={post.id}
-  //         >
-  //           <AspectRatio ratio={1}>
-  //             <NextImage
-  //               src={"https://pbs.twimg.com/media/FKHb6g4VkAAfwvI?format=jpg"}
-  //               layout="fill"
-  //               objectFit="cover"
-  //             />
-  //           </AspectRatio>
-
-  //           <Box p={5}>
-  //             <Box fontSize={"lg"} fontWeight="black">
-  //               {post.keyboard.name}
-  //             </Box>
-  //             <Box fontSize={"md"} fontWeight="semibold">
-  //               {post.keyboard.keycaps} · {post.keyboard.switches}
-  //             </Box>
-  //             <Box fontSize={"sm"} fontWeight="medium">
-  //               by {post.creator.username}
-  //             </Box>
-  //           </Box>
-  //         </Box>
-  //       ))}
-  //     </SimpleGrid>
-  //   );
-  // }
 
   return (
     <>
-      <Text>showing {isLoadingMore ? "..." : totalPosts} post(s) </Text>
-      {posts?.map((posts) => {
-        return posts.result.map((post) => (
-          <div key={post.id}>{post.keyboard.name}</div>
-        ));
-      })}
+      <SimpleGrid minChildWidth={"350px"} spacing={4}>
+        {posts.map((posts) =>
+          posts.result.map((post) => (
+            <Box
+              overflow={"hidden"}
+              borderRadius={"md"}
+              shadow={"md"}
+              key={post.id}
+            >
+              <AspectRatio ratio={1}>
+                <NextImage
+                  src={"https://pbs.twimg.com/media/FKHb6g4VkAAfwvI?format=jpg"}
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </AspectRatio>
+
+              <Box p={5}>
+                <Box fontSize={"lg"} fontWeight="black">
+                  {post.keyboard.name}
+                </Box>
+                <Box fontSize={"md"} fontWeight="semibold">
+                  {post.keyboard.keycaps} · {post.keyboard.switches}
+                </Box>
+                <Box fontSize={"sm"} fontWeight="medium">
+                  by {post.creator.username}
+                </Box>
+              </Box>
+            </Box>
+          ))
+        )}
+      </SimpleGrid>
       <Button
         disabled={isLoadingMore || isReachingEnd}
         onClick={() => setSize(size + 1)}
@@ -86,7 +80,7 @@ const Posts: React.FC<PostsProps> = ({}) => {
         {isLoadingMore
           ? "loading..."
           : isReachingEnd
-          ? "no more issues"
+          ? "no more posts"
           : "load more"}
       </Button>
     </>
