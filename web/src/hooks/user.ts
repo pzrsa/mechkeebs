13b83c013e5210a41362bd80acf4f67250db5b35
parsa <parsamesg@gmail.com>
2022-04-24 22:05:01 +0100

    improve on checking logged out state

--- web/src/hooks/user.ts
+++ web/src/hooks/user.ts
@@ -5,7 +5,7 @@ import { User } from "../types/User";
 export const useUser = () => {
   const { data, error, mutate } = useSWR<User>("me", fetchCurrentUser);
 
-  const loggedOut = error && error.status === 403;
+  const loggedOut = (error && error.status === 403) || !data?.user;
 
   return {
     user: data,
