675f3085e5ee36d13eca2b3299fd9913af33c135
parsa <parsamesg@gmail.com>
2022-04-25 21:45:43 +0100

    create react dropzone for adding image

--- web/src/components/ImageInputField.tsx
+++ web/src/components/ImageInputField.tsx
@@ -1,11 +1,14 @@
 import {
+  Center,
   FormControl,
   FormErrorIcon,
   FormErrorMessage,
   FormLabel,
+  Text,
 } from "@chakra-ui/react";
 import { useField } from "formik";
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import ImageInputThumbnail from "./ImageInputThumbnail";
 
 type ImageInputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
@@ -19,20 +22,38 @@ const ImageInputField: React.FC<ImageInputFieldProps> = ({
 }) => {
   const [field, meta, helpers] = useField(props);
 
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    if (!acceptedFiles.length) {
+      return;
+    }
+    helpers.setValue(acceptedFiles.at(0));
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, isDragAccept } =
+    useDropzone({
+      onDrop,
+      maxFiles: 1,
+      multiple: false,
+      accept: ["image/jpeg", "image/png"],
+    });
+
+  const dropText = isDragActive
+    ? "Drop your image here..."
+    : "Drag 'n' drop the image of your keeb here, or click to select the image";
+
   return (
     <FormControl mb={5} isInvalid={!!(meta.touched && meta.error)}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <input
-        type="file"
-        accept="image/png, image/jpeg"
-        id={field.name}
-        {...field}
-        {...props}
-        value={undefined}
-        onChange={(event) => {
-          helpers.setValue(event.currentTarget.files?.item(0));
-        }}
-      />
+      <Center p={10} cursor="pointer" {...getRootProps()}>
+        <input
+          id={field.name}
+          {...field}
+          {...props}
+          {...getInputProps()}
+          value={undefined}
+        />
+        <Text>{dropText}</Text>
+      </Center>
       {meta.touched && meta.error ? (
         <FormErrorMessage>
           <FormErrorIcon />
