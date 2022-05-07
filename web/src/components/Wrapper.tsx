ad2c9878163f9408fc4f5cfb4a2bb3eb6f65c81a
parsa <parsamesg@gmail.com>
2022-05-07 22:36:01 +0100

    update wrapper max width

--- web/src/components/Wrapper.tsx
+++ web/src/components/Wrapper.tsx
@@ -4,7 +4,12 @@ interface WrapperProps {}
 
 const Wrapper: React.FC<WrapperProps> = ({ children }) => {
   return (
-    <Container maxW="85rem" w="100%" px={1} my={3}>
+    <Container
+      maxW={{ base: "none", md: "60rem", lg: "80rem" }}
+      w="100%"
+      px={1}
+      my={3}
+    >
       {children}
     </Container>
   );
