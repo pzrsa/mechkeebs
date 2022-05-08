ad12d5c136aa74c21bfa683b9513e510b6ae12d9
parsa <parsamesg@gmail.com>
2022-05-08 19:15:35 +0100

    add other favicon types

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -8,13 +8,49 @@ class MechKeebsDocument extends Document {
           <link
             rel="icon"
             type="image/svg+xml"
-            href="/assets/images/favicon/favicon.svg"
+            href="/assets/images/favicons/favicon.svg"
           />
           <link
             rel="icon"
             type="image/png"
-            href="/assets/images/favicon/favicon.png"
+            href="/assets/images/favicons/favicon.png"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/assets/images/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/assets/images/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/assets/images/favicons/favicon-16x16.png"
+          />
+          <link
+            rel="manifest"
+            href="/assets/images/favicons/site.webmanifest"
+          />
+          <link
+            rel="mask-icon"
+            href="/assets/images/favicons/safari-pinned-tab.svg"
+            color="#111111"
+          />
+          <link
+            rel="shortcut icon"
+            href="/assets/images/favicons/favicon.ico"
+          />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta
+            name="msapplication-config"
+            content="/assets/images/favicons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
