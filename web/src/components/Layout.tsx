975e3252665b97892b8c5e87db52b91779a34b95
parsa <parsamesg@gmail.com>
2022-05-13 16:07:16 +0100

    add seo stuff in head tag for every page

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Wrapper from "./Wrapper";
@@ -5,8 +7,41 @@ import Wrapper from "./Wrapper";
 interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+
+  const title = "MechKeebs";
+  const description =
+    "MechKeebs - Home for enthuasists to share their custom keyboards";
+  const image = "/assets/images/favicons/android-chrome-512x512.png";
+
   return (
     <>
+      <Head>
+        <meta name="robots" content="all" />
+        <meta content={description} name="description" />
+        <meta
+          property="og:url"
+          content={`https://mechkeebs.com${router.asPath}`}
+        />
+        <link rel="canonical" href={`https://mechkeebs.com${router.asPath}`} />
+        <meta property="og:site_name" content={title} key="og:site_name" />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} key="og:title" />
+        <meta
+          property="og:image"
+          content={`https://mechkeebs.com${image}`}
+          key="og:image"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@_parsam" />
+        <meta name="twitter:title" content={title} key="twitter:title" />
+        <meta name="twitter:description" content={description} />
+        <meta
+          name="twitter:image"
+          content={`https://mechkeebs.com${image}`}
+          key="twitter:image"
+        />
+      </Head>
       <Navbar />
       <Wrapper>
         {children}
