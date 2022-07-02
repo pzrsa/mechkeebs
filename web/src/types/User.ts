5a440465c56fc430c608019fce427d61c84de91f
parsa <parsamesg@gmail.com>
2022-07-02 17:16:35 +0100

    remove old types

--- web/src/types/User.ts
+++ web/src/types/User.ts
@@ -8,14 +8,3 @@ export interface User {
     updatedAt: string;
   };
 }
-
-export interface LoginFormValues {
-  email: string;
-  password: string;
-}
-
-export interface RegisterFormValues {
-  username: string;
-  email: string;
-  password: string;
-}
