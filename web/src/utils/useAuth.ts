8307f63b961c9f2681d70f019289a9931e041660
parsa <parsamesg@gmail.com>
2022-07-04 22:57:22 +0100

    rename auth hook

--- web/src/utils/useAuth.ts
+++ web/src/utils/useAuth.ts
@@ -2,15 +2,15 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useUser } from "../hooks/user";
 
-const withAuth = () => {
-  const { user, isLoading } = useUser();
+const useAuth = () => {
+  const { user, isLoading, loggedOut } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    if (!user && !isLoading) {
+    if (!user?.user && !isLoading && loggedOut) {
       router.replace("/");
     }
-  }, [isLoading, user, router]);
+  }, [isLoading, user, router, loggedOut]);
 };
 
-export default withAuth;
+export default useAuth;
