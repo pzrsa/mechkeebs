8b835c159807741ffb73a271ed7e44e1e849c4ba
parsa <parsamesg@gmail.com>
2022-07-08 16:47:53 +0100

    add optional input field

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -2,6 +2,7 @@ import {
   FormControl,
   FormErrorIcon,
   FormErrorMessage,
+  FormHelperText,
   FormLabel,
   Input,
 } from "@chakra-ui/react";
@@ -12,12 +13,14 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   placeholder: string;
   label: string;
+  optional?: boolean;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
   size,
   placeholder,
+  optional,
   ...props
 }) => {
   const [field, meta] = useField(props);
@@ -26,6 +29,7 @@ const InputField: React.FC<InputFieldProps> = ({
     <FormControl mb={5} isInvalid={!!(meta.touched && meta.error)}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
       <Input {...field} {...props} id={field.name} placeholder={placeholder} />
+      {optional ? <FormHelperText>Optional</FormHelperText> : null}
       {meta.touched && meta.error ? (
         <FormErrorMessage>
           <FormErrorIcon />
