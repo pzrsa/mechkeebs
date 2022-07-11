ccccacc339ab3c642a399b909dadcce90039e364
parsa <parsamesg@gmail.com>
2022-07-11 21:47:00 +0100

    only rate limit on prod

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import express, { Application } from "express";
 import fileUpload from "express-fileupload";
 import rateLimit from 'express-rate-limit';
 import "reflect-metadata";
-import { CORS_ORIGIN } from "./constants";
+import { CORS_ORIGIN, __prod__ } from "./constants";
 import { AppDataSource } from "./data-source";
 import postsRouter from "./routers/post";
 import usersRouter from "./routers/user";
@@ -23,13 +23,16 @@ const main = async () => {
     fileUpload(),
     cors({ origin: CORS_ORIGIN, credentials: true }),
     cookieParser(process.env.COOKIE_SECRET),
+  );
+
+  __prod__ ? app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
       max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
       standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
       legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     })
-  );
+  ) : null
 
   app.use(usersRouter, postsRouter);
   app.set("json spaces", 2);
