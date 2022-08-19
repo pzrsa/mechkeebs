0617ed3cde7e56bcfe84a1e56e7c09916dae8dd8
parsa <hi@parsam.io>
2022-08-19 12:18:40 +0100

    update favicon to keyboard emoji

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -7,13 +7,7 @@ class MechKeebsDocument extends Document {
         <Head>
           <link
             rel="icon"
-            type="image/svg+xml"
-            href="/assets/images/favicons/favicon.svg"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/assets/images/favicons/favicon.png"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⌨️</text></svg>"
           />
         </Head>
         <body>
